ea16553fe44745730897ab52f414f04fb378f67a
Daria <you@example.com>
2022-08-03 19:27:16 +0300

    add main page in admin panel

--- app/Http/Controllers/Admin/Main/MainController.php
+++ app/Http/Controllers/Admin/Main/MainController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Admin\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
+use App\Models\Culture;
+use App\Models\Fertilizer;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function __invoke() {
-        return view('admin.main.index');
+        $fertilizers = Fertilizer::all()->count();
+        $cultures = Culture::all()->count();
+        $clients = Client::all()->count();
+        $users = User::all()->count();
+        return view('admin.main.index',
+        compact(
+            'fertilizers',
+            'cultures',
+            'clients',
+            'users',
+        ));
     }
 }
--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -15,14 +15,14 @@
                         <!-- small box -->
                         <div class="small-box bg-info">
                             <div class="inner">
-                                <h3>150</h3>
+                                <h3>{{$fertilizers}}</h3>
 
-                                <p>New Orders</p>
+                                <p>Удобрения</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-bag"></i>
+                                <i class="fas fa-frog"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{route('admin.fertilizer.index')}}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <!-- ./col -->
@@ -30,14 +30,14 @@
                         <!-- small box -->
                         <div class="small-box bg-success">
                             <div class="inner">
-                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                <h3>{{$clients}}</h3>
 
-                                <p>Bounce Rate</p>
+                                <p>Клиенты</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-stats-bars"></i>
+                                <i class="fas fa-user-tie"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{route('admin.client.index')}}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <!-- ./col -->
@@ -45,14 +45,14 @@
                         <!-- small box -->
                         <div class="small-box bg-warning">
                             <div class="inner">
-                                <h3>44</h3>
+                                <h3>{{$users}}</h3>
 
-                                <p>User Registrations</p>
+                                <p>Пользователи</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-person-add"></i>
+                                <i class="fas fa-users"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{route('admin.user.index')}}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <!-- ./col -->
@@ -60,14 +60,14 @@
                         <!-- small box -->
                         <div class="small-box bg-danger">
                             <div class="inner">
-                                <h3>65</h3>
+                                <h3>{{$cultures}}</h3>
 
-                                <p>Unique Visitors</p>
+                                <p>Культуры</p>
                             </div>
                             <div class="icon">
-                                <i class="ion ion-pie-graph"></i>
+                                <i class="fas fa-bug"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="{{route('admin.culture.index')}}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <!-- ./col -->
